ad0c3ffead7d5ab7a8e3b1ab7557dad83a299a87
David Journo <david.journo.31@gmail.com>
2024-04-03 09:36:29 +0200

    Adjusting mock functions
    Passing in submissions Array payload instead of links, submissions will now contain submission's Objects, more information can be passed to the generateHtmlEmail function to be reused in the email template

--- utils/mock-email.js
+++ utils/mock-email.js
@@ -1,5 +1,4 @@
 import { generateHtmlEmail, newEmail } from "../lib/tasks";
-import { uuid } from "mu";
 
 /**
  * Creates a mock email for multiple submissions
@@ -7,20 +6,20 @@ import { uuid } from "mu";
  */
 export function multipleSubmissionsMockEmail() {
   const now = new Date().toISOString();
-  const links = [
-    `${process.env.WORSHIP_DECISIONS_APP_BASEURL}search/submissions/${uuid()}`,
-    `${process.env.WORSHIP_DECISIONS_APP_BASEURL}search/submissions/${uuid()}`,
-    `${process.env.WORSHIP_DECISIONS_APP_BASEURL}search/submissions/${uuid()}`,
+  const submissions = [
+    { submissionUri: "http://data.lblod.info/submissions/1041fe53-7d98-46fb-8db9-98a66060498a", decisionTypeLabel: "Afschrift erkenningszoekende besturen" , creatorEenheidLabel: "Kerkfabriek O.-L.-Vrouw van Aalst", sentDate: "2024-03-12T16:00:57.957Z", url: `${process.env.WORSHIP_DECISIONS_APP_BASEURL}search/submissions/1041fe53-7d98-46fb-8db9-98a66060498a`},
+    { submissionUri: "http://data.lblod.info/submissions/1041fe53-7d98-46fb-8db9-98a66060498b", decisionTypeLabel: "Jaarrekening" , creatorEenheidLabel: "Kerkfabriek St.-Martinus van Aalst", sentDate: "2023-04-01T11:00:23.803Z", url: `${process.env.WORSHIP_DECISIONS_APP_BASEURL}search/submissions/1041fe53-7d98-46fb-8db9-98a66060498b`},
+    { submissionUri: "http://data.lblod.info/submissions/1041fe53-7d98-46fb-8db9-98a66060498c", decisionTypeLabel: "Opstart beroepsprocedure naar aanleiding van een beslissing" , creatorEenheidLabel: "Kerkfabriek H. Hart van Aalst", sentDate: "2024-01-10T09:00:00.398Z", url: `${process.env.WORSHIP_DECISIONS_APP_BASEURL}search/submissions/1041fe53-7d98-46fb-8db9-98a66060498c`}
   ];
   let email = newEmail(
     "me@redpencil.io",
     "you@redpencil.io",
-    `${links.length} Nieuwe inzendingen - ${now}`,
+    `${submissions.length} Nieuwe inzendingen - ${now}`,
     `content ${now}`
   );
   email.uri = `http://data.lblod.info/id/emails/${email.uuid}`;
   email.bcc = "blindcc@redpencil.io";
-  email.htmlContent = generateHtmlEmail("foo", links);
+  email.htmlContent = generateHtmlEmail("Aalst", submissions);
   return email;
 }
 
@@ -30,17 +29,15 @@ export function multipleSubmissionsMockEmail() {
  */
 export function singleSubmissionMockEmail() {
   const now = new Date().toISOString();
-  const link = [`${
-    process.env.WORSHIP_DECISIONS_APP_BASEURL
-  }search/submissions/${uuid()}`];
+  const submissions = [{ submissionUri: "http://data.lblod.info/submissions/40500896-8e52-4660-883e-534ec511384b", decisionTypeLabel: "Opstart beroepsprocedure naar aanleiding van een beslissing" , creatorEenheidLabel: "Kerkfabriek H. Hart van Aalst", sentDate: "2024-01-10T09:00:00.398Z", url: `${process.env.WORSHIP_DECISIONS_APP_BASEURL}search/submissions/40500896-8e52-4660-883e-534ec511384b`}];
   let email = newEmail(
     "me@redpencil.io",
     "you@redpencil.io",
-    `DossierType - Nieuwe inzendingen - ${now}`,
+    `DossierType ${submissions[0].decisionTypeLabel} - Nieuwe inzending - ${now}`,
     `content ${now}`
   );
   email.uri = `http://data.lblod.info/id/emails/${email.uuid}`;
   email.bcc = "blindcc@redpencil.io";
-  email.htmlContent = generateHtmlEmail("foo", link);
+  email.htmlContent = generateHtmlEmail("Aalst", submissions);
   return email;
 }
